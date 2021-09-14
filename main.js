9e2aa5ea095370dc204f589a9a62b30d5dd5addd
Senay Yakut <senaykt@gmail.com>
2021-09-13 22:25:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 //Determine if a string is a palindrome
 
+function isTextPalindrome(text){
+  if(text === undefined){
+    return false;
+  }
+  let left = 0;
+  let right = text.length -1;
+  while(left < right){
+    if(text[left++] !== text[right--]){
+      return false;
+    }
+  }
+  return true;
+}
+
+function isPhrasePalindrome(text){
+  let chars = text.replace(/[^a-zA-Z]/g, "").toLowerCase();
+  return isTextPalindrome(chars);
+
+}
+
+console.log(isTextPalindrome("sees"));
+console.log(isPhrasePalindrome("madam"));
