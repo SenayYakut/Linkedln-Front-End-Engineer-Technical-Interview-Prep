310ce44aa45e143fb2af4c5d8f276ce195a86647
Senay Yakut <senaykt@gmail.com>
2021-09-21 10:00:58 -0700

    Update longestPalindromicSubstring.js
    Time complexity n^2: Quadratic
    space complexity 0(1):Constant

--- longestPalindromicSubstring.js
+++ longestPalindromicSubstring.js
@@ -1,4 +1,25 @@
 //Given a string s, return the longest palindromic substring in s.
+var longestPalindrome = function(s) {
+  let startIndex = 0;
+  let maxLength = 1;
 
+  function expandAround(left,right){
+    while(left >=0 && right< s.length && s[left] === s[right]){
+      let currentLength = right - left +1;
+      if(currentLength > maxLength){
+        maxLength = currentLength;
+          startIndex = left;
+      }
+    right++;
+    left--;
+    }
+  }
+  for(let i =0; i<s.length; i++){
+    expandAround(i-1,i+1);
+    expandAround(i,i+1);
+  }
+  return s.slice(startIndex, startIndex + maxLength);
 
- 
+}
+
+console.log(longestPalindrome("aba"));
