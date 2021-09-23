e18594d34a07266f925822133ec16b2e782ce71d
Senay Yakut <senaykt@gmail.com>
2021-09-22 20:09:09 -0700

    Update containsDuplicates.js
    Time Complexity = O(N): Linear
    Space Complexity = O(N): Linear

--- containsDuplicates.js
+++ containsDuplicates.js
@@ -1,2 +1,14 @@
 //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+var containsDuplicate = function(nums) {
+    let visitedNumber ={};
+    for(let i = 0; i< nums.length; i++){
+      let num = nums[i];
+      if(visitedNumber[num]){
+        return true;
+      }else{
+        visitedNumber[num] = true;
+      }   
+    }
+    return false;
+};
